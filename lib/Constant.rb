3090c34df6d11f86daf95072ee29164eb4afcae9
浦野　頌平 <s_urano@dadway.com>
2018-08-15 12:59:59 +0900

    Gsuiteのgroupクラスを作り替え

--- lib/Constant.rb
+++ lib/Constant.rb
@@ -1,33 +1,44 @@
-#定数
+#ドメイン
 DOMAIN = '@dadway.com'
+#会議体アドレス
 ALL = 'all@dadway.com'
-ALL_NAME = 'ALL'
-ALL_DESCRIPTION = 'Whole Company Common'
 EXECUTIVE = 'executive@dadway.com'
-EXECUTIVE_NAME = 'EXECUTIVE'
-EXECUTIVE_DESCRIPTION = 'Executive Conference'
 DETERMINATION = 'determination@dadway.com'
+#会議体名
+ALL_NAME = 'ALL'
+EXECUTIVE_NAME = 'EXECUTIVE'
 DETERMINATION_NAME = 'DETERMINATION'
+#グループ概要
+ALL_DESCRIPTION = 'Whole Company Common'
 DETERMINATION_DESCRIPTION = 'Determination Report Conference'
+EXECUTIVE_DESCRIPTION = 'Executive Conference'
 ORGANIZATION_DESCRIPTION = 'Organization'
 INTERNAL_DESCRIPTION = 'Internal Office'
 EXTERNAL_DESCRIPTION = 'External Office'
+#その他
+GROUP_REFERENSE = 'ANYONE_CAN_POST'
+SHOW_DIRECTORY = 'true'
 PASSWORD = 'Dad880188'
 HEAD = 'DW_'
 MEMBER_ROLE = 'MEMBER'
-#EMPLOYEE_FILE_NAME = '/mnt/gsuite/temporary/urano/社員情報.xlsx'
-EMPLOYEE_FILE_NAME = './tmp/社員情報.xlsx'
-INTERNAL_FILE_NAME = '/mnt/gsuite/open/01_全社共通/GSuite_社内限定用アドレス_中村和寛/社内限定用アドレス管理表.xlsx'
-EXTERNAL_SHOP_FILE_NAME = '/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表（店舗用）.xlsx'
-EXTERNAL_PUBLIC_FILE_NAME = '/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表.xlsx'
-ORGUNIT = '/Gmail、HOのみ'
-#MANAGEMENT_GROUP = 'ICTG'
-#PUBLIC_RELATION_GROUP = 'PRAG'
-#WEB_GROUP = 'OSG'
-
-#認証
+#ファイルパス
+EMPLOYEE_FILE_NAME = ['./tmp/社員情報.xlsx']
+#INTERNAL_FILE_NAME = '/mnt/gsuite/open/01_全社共通/GSuite_社内限定用アドレス_中村和寛/社内限定用アドレス管理表.xlsx'
+INTERNAL_FILE_NAME = ['C:/Users/s_urano/Desktop/GSuite_アドレス帳/社内限定用アドレス管理表.xlsx']
+#EXTERNAL_SHOP_FILE_NAME = '/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表（店舗用）.xlsx'
+#EXTERNAL_PUBLIC_FILE_NAME = '/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表.xlsx'
+#EXTERNAL_FILE_NAME = ['/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表（店舗用）.xlsx',
+#                    '/mnt/gsuite/close/ADIV/ADIV_Share/GSuite_アドレス帳/外部公開用アドレス管理表.xlsx']
+EXTERNAL_FILE_NAME = ['C:/Users/s_urano/Desktop/GSuite_アドレス帳/外部公開用アドレス管理表（店舗用）.xlsx',
+                        'C:/Users/s_urano/Desktop/GSuite_アドレス帳/外部公開用アドレス管理表.xlsx']
+#組織部門
+ORGUNIT = '/全許可'
+ORGUNIT_ICTG = '/ICTG'
+MANAGEMENT_GROUP = 'ICTG'
+#認証関連
 OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 APPLICATION_NAME = 'Directory API Ruby Quickstart'
+#CLIENT_SECRETS_PATH = '/script/etc/client_secret.json'
 CLIENT_SECRETS_PATH = './etc/client_secret.json'
 CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
                              "admin-directory_v1.yaml")
--- lib/GsuiteGroup.rb
+++ lib/GsuiteGroup.rb
@@ -1,11 +1,69 @@
 require '/script/lib/Gsuite.rb'
 require '/script/lib/Auth.rb'
 
-class GsuiteGroup < Gsuite
+class Group < Gsuite
 
   def initialize
-    super
-    @group_response = @service.list_groups(customer: 'my_customer')
+    self.directory_auth
+    self.groups_settings_auth
+  end
+
+  def get_groups
+
+    gsuite_groups = Array.new
+    pagetoken = ""
+
+    loop do
+      list = @directory_auth.list_groups(customer: 'my_customer', page_token: "#{pagetoken}")
+      list.groups.each{|group| gsuite_groups << group.email}
+      pagetoken = list.next_page_token
+      break if pagetoken.nil?
+    end
+
+    gsuite_groups
+
+  end
+
+  def create_groups(excel_groups)
+
+    gsuite_groups = self.get_groups
+
+    excel_groups.each{|excel_group| 
+      next unless gsuite_groups.include?("#{HEAD}" + "#{excel_group['mail']}")
+
+      begin
+        group = Google::Apis::AdminDirectoryV1::Group.new(
+          email: "#{excel_group["mail"]}",
+          name: "#{HEAD}#{excel_group["name"]}",
+          description: "#{excel_group["description"]}"
+        )
+        @directory_auth.insert_group(group)
+      rescue => exception
+        Log.error("グループの作成でエラーが発生しました。#{excel_group['name']}:#{excel_group['mail']}")
+        Log.error("#{exception}")
+        SendMail.error("グループの作成でエラーが発生しました。#{excel_group['name']}:#{excel_group['mail']}\n#{exception}")
+      else
+        Log.info("グループを作成しました。#{excel_group['name']}:#{excel_group['mail']}")
+      end
+
+      begin
+        group_setting = patch_service.get_group("#{excel_group["mail"]}")
+        next if  group_setting.who_can_post_message == "#{GROUP_REFERENSE}" && group_setting.show_in_group_directory == true
+
+        setting = Google::Apis::GroupssettingsV1::Groups.new(
+          who_can_post_message: "#{GROUP_REFERENSE}",
+          show_in_group_directory: "#{SHOW_DIRECTORY}"
+        )
+        #@groups_settings_auth.patch_group("#{excel_group["mail"]}", setting)
+      rescue => exception
+        Log.error("グループの設定変更でエラーが発生しました。#{excel_group['name']}:#{excel_group['mail']}")
+        Log.error("#{exception}")
+        SendMail.error("グループの設定変更でエラーが発生しました。#{excel_group['name']}:#{excel_group['mail']}\n#{exception}")
+      else
+        Log.info("グループの設定変更をしました。#{excel_group['name']}:#{excel_group['mail']}")
+      end
+    }
+
   end
 
   def check(group)
