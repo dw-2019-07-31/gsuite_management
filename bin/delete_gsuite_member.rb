2f0ca7804e86760127b45f050188b66eb24f6998
s_urano <s_urano@dadway.com>
2019-06-12 16:58:18 +0900

    組織メンバー削除修正

--- bin/delete_gsuite_member.rb
+++ bin/delete_gsuite_member.rb
@@ -3,6 +3,7 @@ require 'google/apis/groupssettings_v1'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'fileutils'
+require './lib/Constant.rb'
 require './lib/ExcelGroup.rb'
 require './lib/GsuiteGroup.rb'
 require './lib/Log.rb'
@@ -11,11 +12,12 @@ Log.instance
 gsuite = Group.instance
 excel = Organization.instance
 
-excel_groups = excel.get_group_list
+gsuite_groups = gsuite.get_groups(head:"#{HEAD}")
 
-excel_groups.each{|excel_group|
+gsuite_groups.each{|gsuite_group|
   excel_members = Array.new
-  excel_members = excel.get_members(excel_group)
-  next unless excel_members.empty?
-  gsuite.delete_members(excel_group, excel_members)
+  gsuite_members = gsuite.get_members(gsuite_group['mail'])
+  next if gsuite_members.empty?
+  excel_members = excel.get_members(gsuite_group['mail'])
+  gsuite.delete_members(gsuite_members, excel_members, gsuite_group)
 }
